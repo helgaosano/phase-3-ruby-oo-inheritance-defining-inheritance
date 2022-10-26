91f0fcf1f05af2f7a294cece47360cb3126156b5
tania <taniaosano15@gmail.com>
2022-10-26 14:13:16 +0300

    Code Along

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,11 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+#class Car inherited from Vehicle
+class Car < Vehicle
+
+    #overwriting the inherited go method
+    def go
+      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+  end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,19 @@
 class Vehicle
 
-end
+    attr_accessor :wheel_size, :wheel_number
+  
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end
+  
+    #instance methods describing common vehicle behaviors
+    def go
+      "vrrrrrrrooom!"
+    end
+  
+    def fill_up_tank
+      "filling up!"
+    end
+  
+  end
